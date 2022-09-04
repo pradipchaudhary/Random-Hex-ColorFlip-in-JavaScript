1b35396da74f017b4bff2de3780f1ab943483a1d
Pradip Chaudhary <chaudharypradip678@gmail.com>
2022-09-04 15:38:33 +0545

    add hex color code

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 console.log("Welcome");
 
-const colors = ["red", "green", "yellow", "#979797"];
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 
 const color = document.querySelector("#color");
 const btn = document.querySelector("#btn");
 
 btn.addEventListener("click", function () {
-  const randomNumber = getRandomNumber();
-  console.log(randomNumber);
+  let haxColor = "#";
 
-  document.body.style.background = colors[randomNumber];
-  color.textContent = colors[randomNumber];
+  for (let i = 0; i < 6; i++) {
+    haxColor += hex[getRandomNumber()];
+    console.log(i);
+  }
+  color.textContent = haxColor;
+  document.body.style.backgroundColor = haxColor;
 });
 
 // Get random number
 function getRandomNumber() {
-  return Math.floor(Math.random() * colors.length);
+  return Math.floor(Math.random() * hex.length);
 }
